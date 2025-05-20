177480542083a4c9877a621b34395ccc31e63079
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-20 13:56:18 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 var h1 = document.getElementsByTagName('h1');
 var changeText = function () {
   var p = document.createElement('p');
-  var random = Math.random();
-  p.innerHTML = random;
-  h1[0].after(p); 
+  var q = document.createElement('q');
+  var random_1 = Math.random();
+  var random_2 = Math.random();
+  p.innerHTML = random_1+"+"+random_2;
+  q.innerHTML = random_1+random_2
+  h1[0].after(p);
+  h1[1].after(q)
 }
 h1[0].addEventListener('click', changeText);
