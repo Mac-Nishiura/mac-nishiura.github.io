bfe0c4783d8ec7490a73836c40802e57debb8b0a
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-20 19:34:06 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ var changeText = function () {
   var q = document.createElement('q');
   var random_1 = Math.floor(Math.random()*10);
   var random_2 = Math.floor(Math.random()*10);
+  var random_3 = Math.floor(Math.random()*2+1);
   p.innerHTML = random_1+" X "+random_2;
-  q.innerHTML = random_1*random_2
+  q.innerHTML = (random_1*random_2)*random_3; 
   h1[0].after(p);
   h1[1].after(q)
 }
 var removeText = function () {
-  let pp = document.getElementsById("p");
-  let qq = document.getElementsById("q");
+  var pp = document.getElementsByTagName('p');
+  var qq = document.getElementsByTagName('q');
   pp.remove();
-  qq.remove();
+  qq.remove()
 }
 h1[0].addEventListener('click', changeText);
 h1[1].addEventListener('click', removeText);
