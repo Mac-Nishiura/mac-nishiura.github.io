7c15416a3b6385db5154e80fc3d8fb3e9e777275
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-20 13:38:57 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var h1 = document.getElementsByTagName('h1');
 var changeText = function () {
   var p = document.createElement('p');
-  p.innerHTML = 'Javascriptって面白い！';
-  h1[0].after(p);
+  var random = Math.random();
+  p.innerHTML = random;
+  h1[0].after(p); 
 }
 h1[0].addEventListener('click', changeText);
