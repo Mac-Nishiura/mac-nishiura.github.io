5342d69390356f9b1e79052457ab7f96679ea525
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-20 18:03:31 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var h1 = document.getElementsByTagName('h1');
 var changeText = function () {
-  var pp = document.getElementByTagName('p');
-  var qq = cocument.getElementByTagName('q');
+  let pp = document.getElementByTagName('p');
+  let qq = cocument.getElementByTagName('q');
   pp.remove();
   qq.remove();
   var p = document.createElement('p');
