1f2e185a3e6d8da9a25fbb52da58286fdf171aa1
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-20 14:06:47 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var h1 = document.getElementsByTagName('h1');
 var changeText = function () {
   var p = document.createElement('p');
   var q = document.createElement('q');
-  var random_1 = Math.random();
-  var random_2 = Math.random();
-  p.innerHTML = random_1+"+"+random_2;
-  q.innerHTML = random_1+random_2
+  var random_1 = Math.floor(Math.random()*10);
+  var random_2 = Math.floor(Math.random()*10);
+  p.innerHTML = random_1+" X "+random_2;
+  q.innerHTML = random_1*random_2
   h1[0].after(p);
   h1[1].after(q)
 }
