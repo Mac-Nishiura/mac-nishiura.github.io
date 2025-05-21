fb4466ceee7cf3d9f420aedca39c799ed50dec09
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-05-21 10:11:06 -0400

    Create main1.js

---
+++ js/main1.js
@@ -0,0 +1,20 @@
+var h6 = document.getElementsByTagName('h6');
+var changeText = function () {
+  var p = document.createElement('p');
+  var q = document.createElement('q');
+  var random_1 = Math.floor(Math.random()*10);
+  var random_2 = Math.floor(Math.random()*10);
+  var random_3 = Math.floor(Math.random()*3);
+  p.innerHTML = random_1*random_2+" / "random_1;
+  q.innerHTML = random_2+random_3; 
+  h6[0].after(p);
+  h6[1].after(q)
+}
+var removeText = function () {
+  var pp = document.getElementsByTagName('p');
+  var qq = document.getElementsByTagName('q');
+  pp.innerHTML.remove();
+  qq.innerHTML.remove()
+}
+h6[0].addEventListener('click', changeText);
+h6[1].addEventListener('click', removeText);
