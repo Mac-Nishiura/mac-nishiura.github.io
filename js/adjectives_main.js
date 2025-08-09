63e703923427a373202795d18be39144b9bbb8e5
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-08-09 09:13:35 -0400

    Update adjectives_main.js

--- js/adjectives_main.js
+++ js/adjectives_main.js
@@ -1,19 +1,20 @@
-let english_adjectives = ["HIGH","LOW","THICK","THIN","BIG","SMALL","MUCH","LITTLE","NEW","OLD"]
-let picture_adjectives = ["picture1.jpg","picture2.jpg","picture3.jpg","picture4.jpg","picture5.jpg","picture6.jpg","picture7.jpg","picture8.jpg","picture9.jpg","picture10.jpg"]
-let japanese_adjectives = ["TAKAI","HIKUI","ATSUI","USUI","OHKII","CHIISAI","OHI","SUKUNAI","ATARASHII","FURUI"]
+let english_adjectives = ["HIGH","LOW","THICK","THIN","BIG","SMALL","MUCH","LITTLE","NEW","OLD"];
+let picture_adjectives = ["Picture1.jpg","Picture2.jpg","Picture3.jpg","Picture4.jpg","Picture5.jpg","Picture6.jpg","Picture7.jpg","Picture8.jpg","Picture9.jpg","Picture10.jpg"];
+let japanese_adjectives = ["TAKAI","HIKUI","ATSUI","USUI","OHKII","CHIISAI","OHI","SUKUNAI","ATARASHII","FURUI"];
 var h6 = document.getElementsByTagName('h6');
+
 var changeEnglish = function() {
-  var random = Math.floor(Math.random()*10)+1;
+  var rand = Math.floor(Math.random()*10)+1;
   var p = document.createElement('p');
-  p.innerHTML = english_adjectives[random]; 
+  p.innerHTML = english_adjectives[rand]; 
   h6[0].after(p);
-  setTimeOut(changeJapanese(random);
+  return rand;
 }
 function changeJapanese(num) {
   var q = document.createElement('q');
   q.innerHTML = japanese_adjectives[num]; 
   h6[2].after(q);
-  return
+  return;
 }
 var removeText = function () {
   const pp = document.querySelector('p');
@@ -25,13 +26,12 @@ var removeText = function () {
 let question = document.getElementById('btn');
 let answer = document.getElementById('bbttnn');
 let erase = document.getElementById('bbbtttnnn')
-
-rand = changeEnglish() 
+ 
 
 question.addEventListener('click', changeEnglish);
 answer.addEventListner('click', changeJapanese(rand));
 erase.addEventListener('click', removeText);
 
 h6[0].addEventListener('click', changeEnglish);
-h6[1].addEventListener('click', changeJapanese(rand));
+h6[1].addEventListener('click', changeJapanese(changeEnglish());
 h6[2].addEventListener('click', removeText);
