ec7e543ca580e3b1553beb1043f8bdea765c6b14
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-08-08 21:28:35 -0400

    Update adjectives_main.js

--- js/adjectives_main.js
+++ js/adjectives_main.js
@@ -1,1 +1,34 @@
+english_adjectives = {"HIGH","LOW","THICK","THIN","BIG","SMALL","MUCH","LITTLE","NEW","OLD"}
+picture_adjectives = {"picture1.jpg","picture2.jpg","picture3.jpg","picture4.jpg","picture5.jpg","picture6.jpg","picture7.jpg","picture8.jpg","picture9.jpg","picture10.jpg"}
+japanese_adjectives = {"TAKAI","HIKUI","ATSUI","USUI","OHKII","CHIISAI","OHI","SUKUNAI","ATARASHII","FURUI"}
+var h6 = document.getElementsByTagName('h6');
+var changeEnglish = function (num) {
+  var p = document.createElement('p');
+  p.innerHTML = english_adjectives[num]; 
+  h6[0].after(p);
+}
+var changeJapanese = function (num) {
+  var q = document.createElement('q');
+  q.innerHTML = japanese_adjectives[num]; 
+  h6[1].after(q);
+}
+var removeText = function () {
+  const pp = document.querySelector('p');
+  const qq = document.querySelector('q');
+  pp.remove();
+  qq.remove()
+}
 
+let question = document.getElementById('btn');
+let answer = document.getElementById('bbttnn');
+let erase = document.getElementById('bbbtttnnn')
+
+var random = Math.floor(Math.random()*10)+1
+
+question.addEventListener('click', changeEnglish(random));
+answer.addEventListner('click', changeJapanese(random));
+erase.addEventListener('click', removeText);
+
+h6[0].addEventListener('click', changeEnglish(i));
+h6[1].addEventListener('click', changeJapanese(i));
+h6[2].addEventListener('click', removeText);
