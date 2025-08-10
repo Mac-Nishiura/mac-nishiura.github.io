7d05da73e38ca662b0b3441cf2345e645039c1ce
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-08-10 17:41:47 -0400

    Update adjectives_main.js

--- js/adjectives_main.js
+++ js/adjectives_main.js
@@ -12,7 +12,7 @@ function changeJapanese(num) {
 }
 
 function changeEnglish() {
-  rand = Math.floor(Math.random()*10);
+  rand = Math.floor(Math.random()*20);
   var p = document.createElement('p');
   var q = document.createElement('img');
   p.innerHTML = english_adjectives[rand];
