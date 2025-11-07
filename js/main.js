d7b581b75ac6fdc60b9b5ac5e955cbe0b7277d3f
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-11-07 17:31:42 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 var h6 = document.getElementsByTagName('h6');
 var changeText = function () {
   var p = document.createElement('p');
-  var q = document.createElement('q');
+  /*var q = document.createElement('q');*/
   var random_1 = Math.floor(Math.random()*10);
   var random_2 = Math.floor(Math.random()*10);
-  var random_3 = Math.floor(Math.random()*3);
+  /*var random_3 = Math.floor(Math.random()*3);*/
   p.innerHTML = random_1+" X "+random_2;
-  q.innerHTML = random_1*random_2+random_3; 
+  /*q.innerHTML = random_1*random_2+random_3;*/ 
   h6[0].after(p);
-  h6[1].after(q)
+  /*h6[1].after(q)*/
 }
 var removeText = function () {
   const pp = document.querySelector('p');
-  const qq = document.querySelector('q');
+  /*const qq = document.querySelector('q');*/
   pp.remove();
-  qq.remove()
+  /*qq.remove()*/
 }
 
 let quest = document.getElementById('btn');
@@ -24,4 +24,4 @@ quest.addEventListener('click', changeText);
 erase.addEventListener('click', removeText);
 
 h6[0].addEventListener('click', changeText);
-h6[1].addEventListener('click', removeText);
+/*h6[1].addEventListener('click', removeText);*/
