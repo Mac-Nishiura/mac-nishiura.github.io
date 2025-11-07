5d8d37d8a6991b442f3bf88737dd833b98419db9
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2025-11-07 18:05:21 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 var h6 = document.getElementsByTagName('h6');
 var changeText = function () {
   var p = document.createElement('p');
-  /*var q = document.createElement('q');*/
+  var q = document.createElement('q');
   var random_1 = Math.floor(Math.random()*9+1);
   var random_2 = Math.floor(Math.random()*9+1);
   /*var random_3 = Math.floor(Math.random()*3);*/
   p.innerHTML = random_1+" X "+random_2;
-  /*q.innerHTML = random_1*random_2+random_3;*/ 
+  q.innerHTML = random_1*random_2 
   h6[0].after(p);
-  /*h6[1].after(q)*/
+  h6[1].after(q)
+}
+var answerText = function () {
+
 }
 var removeText = function () {
   const pp = document.querySelector('p');
-  /*const qq = document.querySelector('q');*/
+  const qq = document.querySelector('q');
   pp.remove();
-  /*qq.remove()*/
+  qq.remove()
 }
 
 let quest = document.getElementById('btn');
