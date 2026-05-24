4721140880b424746363f1c564661ef86c61f12d
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2026-05-24 09:22:09 -0400

    Update verbs_main.js

--- js/verbs_main.js
+++ js/verbs_main.js
@@ -1,11 +1,11 @@
 var english_verbs = ['WALK','RUN','EAT','DRINK','WRITE','READ','LISTEN','SPEAK','LOOK','SING','DANCE','SLEEP','WAKE UP','CLEAN','WASH','SWIM','KICK','THROW','HIT','LAUGH'];
 var picture_verbs = ['Picture1.png','Picture2.png','Picture3.png','Picture4.png','Picture5.png','Picture6.png','Picture7.png','Picture8.png','Picture9.png','Picture10.png','Picture11.png','Picture12.png','Picture13.png','Picture14.png','Picture15.png','Picture16.png','Picture17.png','Picture18.png','Picture19.png','Picture20.png'];
 var japanese_verbs = ['ARUKU','HASHIRU','TABERU','NOMU','KAKU','YOMU','KIKU','HANASU','MIRU','UTAU','ODORU','NERU','OKIRU','SOHJISURU','SENTAKUSURU','OYOGU','KERU','NAGERU','UTSU','WARAU'];
-var h2 = document.getElementsByTagName('h2');
+var h1 = document.getElementsByTagName('h1');
 
 function changeJapanese(num) {
   r.innerHTML = japanese_verbs[num]; 
-  h2[3].after(r);
+  h1[3].after(r);
   r.style.color = 'white';
   return;
 }
@@ -16,15 +16,15 @@ function changeEnglish() {
     p.innerHTML = english_verbs[rand];
     q.src = `../photos2/${picture_verbs[rand]}`;
     q.width = 100; 
-    h2[0].after(p);
-    h2[1].after(q);
+    h1[0].after(p);
+    h1[1].after(q);
     setTimeout(changeJapanese(rand),5000);
   } else {
     p.innerHTML = english_verbs[i];
     q.src = `../photos2/${picture_verbs[i]}`;
     q.width = 100; 
-    h2[0].after(p);
-    h2[1].after(q);
+    h1[0].after(p);
+    h1[1].after(q);
     setTimeout(changeJapanese(i),5000);
     i++;
   }
@@ -74,5 +74,5 @@ question.addEventListener('click', changeEnglish);
 answer.addEventListener('click', answerJapanese); 
 erase.addEventListener('click', removeText);
 
-h2[0].addEventListener('click', changeEnglish);
-h2[3].addEventListener('click', answerJapanese);
+h1[0].addEventListener('click', changeEnglish);
+h1[3].addEventListener('click', answerJapanese);
