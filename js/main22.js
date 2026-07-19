a8b4060d50a43c0797373e23e814553b907e877b
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2026-07-19 13:20:54 -0400

    Update and rename main2.js to main22.js

--- js/main22.js
+++ js/main22.js
@@ -4,10 +4,15 @@ var changeText = function () {
   var p = document.createElement('p');
   var q = document.createElement('q');
   var random_1 = Math.floor(Math.random()*99+1);
-  var random_2 = Math.floor(Math.random()*9+1);
+  var random_2 = Math.floor(Math.random()*99+1);
   /*var random_3 = Math.floor(Math.random()*9+1);*/
-  p.innerHTML = random_1+" - "+random_2;
-  q.innerHTML = random_1-random_2; 
+  if (random_1-random2)<0 {
+    p.innerHTML = random_2+" - "+random_1;
+    q.innerHTML = random_2-random_1; 
+  } else {
+    p.innerHTML = random_1+" - "+random_2;
+    q.innerHTML = random_1-random_2; 
+  }
   h6[0].after(p);
   p.style.fontSize = "240px";
   h6[1].after(q);
