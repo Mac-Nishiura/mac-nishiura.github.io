1a7cddb2973a23e6015121af51a9022d4fb2fc7f
Mac-Nishiura <136400151+Mac-Nishiura@users.noreply.github.com>
2026-08-18 05:37:32 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var changeText = function () {
   p.innerHTML = random_1+" × "+random_2;
   q.innerHTML = random_1*random_2; 
   h3[0].after(p);
-  p.style.fontSize = "240px";
+  /*p.style.fontSize = "240px";*/
   h3[1].after(q);
   q.style.color = "white";
   return
